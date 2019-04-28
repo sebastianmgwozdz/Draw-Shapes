258e986dfaae27e464932609ba70e00dfcf599b1
Sebastian <sebastianmgwozdz@gmail.com>
2019-04-27 23:20:54 -0700

    first commit

---
+++ Draw_Shapes/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Draw_Shapes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(1, 44);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1383, 819);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Panel1_MouseClick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Red",
+            "Blue",
+            "Black",
+            "Orange",
+            "Yellow",
+            "Green",
+            "Purple"});
+            this.comboBox1.Location = new System.Drawing.Point(113, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.ComboBox1_SelectedValueChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Rectangle",
+            "Circle"});
+            this.comboBox2.Location = new System.Drawing.Point(314, 13);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.SelectedValueChanged += new System.EventHandler(this.ComboBox2_SelectedValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(508, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Undo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(650, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Redo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(791, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1384, 861);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.panel1);
+            this.MaximumSize = new System.Drawing.Size(1400, 900);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Draw Shapes";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Draw_Shapes/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Draw_Shapes
+{
+    public partial class Form1 : Form
+    {
+        Color color;
+        string shape;
+        Stack<MyShape> undoHistory = new Stack<MyShape>();
+        Stack<MyShape> redoHistory = new Stack<MyShape>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Select Color
+        private void ComboBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            string colorName = Convert.ToString(comboBox1.SelectedItem);
+            color = Color.FromName(colorName);
+            
+        }
+
+        // Draw Shape
+        private void Panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            SolidBrush sb = new SolidBrush(color);
+            Graphics g = panel1.CreateGraphics();
+            Rectangle r = new Rectangle(new Point(e.X - 50, e.Y - 50), new
+                Size(100, 100));
+            try
+            {
+                if (shape.Equals("Rectangle"))
+                {
+                    g.FillRectangle(sb, r);
+                }
+                else
+                {
+                    g.FillEllipse(sb, r);
+                }
+                button1.Enabled = true;
+                undoHistory.Push(new MyShape(shape, r, color));
+
+            } catch (NullReferenceException)
+            {
+
+            }
+            redoHistory = new Stack<MyShape>();
+            button2.Enabled = false;
+        }
+
+        // Select Shape
+        private void ComboBox2_SelectedValueChanged(object sender, EventArgs e)
+        {
+            shape = Convert.ToString(comboBox2.SelectedItem);
+        }
+
+        // Undo Button
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+            g.Clear(BackColor);
+
+            redoHistory.Push(undoHistory.Pop());
+            Stack<MyShape> copy = new Stack<MyShape>(undoHistory);
+            while (copy.Count > 0)
+            {
+                MyShape sh = copy.Pop();
+                SolidBrush sb = new SolidBrush(sh.Col);
+                if (sh.Shape.Equals("Rectangle"))
+                {
+                    g.FillRectangle(sb, sh.Rectangle);
+                }
+                else
+                {
+                    g.FillEllipse(sb, sh.Rectangle);
+                }
+            }
+            if (undoHistory.Count == 0)
+            {
+                button1.Enabled = false;
+            }
+            button2.Enabled = true;
+        }
+
+        // Redo Button
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+
+            MyShape sh = redoHistory.Pop();
+            undoHistory.Push(sh);
+            SolidBrush sb = new SolidBrush(sh.Col);
+            if (sh.Shape.Equals("Rectangle"))
+            {
+                g.FillRectangle(sb, sh.Rectangle);
+            }
+            else
+            {
+                g.FillEllipse(sb, sh.Rectangle);
+            }
+
+            if (redoHistory.Count == 0)
+            {
+                button2.Enabled = false;
+            }
+        }
+
+        // Reset Button
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+            g.Clear(BackColor);
+
+            undoHistory = new Stack<MyShape>();
+            redoHistory = new Stack<MyShape>();
+
+            button1.Enabled = false;
+            button2.Enabled = false;
+        }
+    }
+
+    public class MyShape
+    {
+        public string Shape { get; }
+        public Rectangle Rectangle { get; }
+        public Color Col { get; }
+        public MyShape(string shape, Rectangle rectangle, Color color)
+        {
+            this.Shape = shape;
+            this.Rectangle = rectangle;
+            this.Col = color;
+        }
+    }
+}
